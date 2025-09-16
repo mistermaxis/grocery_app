743451feb581dc9b9275d73cf0e6e25e4dedccd9
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-09-16 14:30:37 -0300

    Add provider wrapper to main app widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/models/cart_model.dart';
 import 'package:grocery_app/pages/intro_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   //debugPaintSizeEnabled = true;
@@ -13,15 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Grocery App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        textTheme: GoogleFonts.notoSansTextTheme(
-          Theme.of(context).textTheme.apply(),
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MaterialApp(
+        title: 'Grocery App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          textTheme: GoogleFonts.notoSansTextTheme(
+            Theme.of(context).textTheme.apply(),
+          ),
         ),
+        home: IntroPage(),
       ),
-      home: IntroPage(),
     );
   }
 }
