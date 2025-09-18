6760efbe26f49bdb24f5c19e97f60b07daa270e4
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-09-18 01:57:57 -0300

    Create ButtonContainer widget

---
+++ lib/pages/intro_page/hero_button/components/button_container.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ButtonContainer extends StatelessWidget {
+  const ButtonContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 25, 92, 23),
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Text(
+        style: TextStyle(color: Colors.white, fontSize: 20),
+        'Get Started ⚡',
+      ),
+    );
+  }
+}
