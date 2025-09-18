9503f76f8efd8fa00d68a108274520b93f824246
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-09-18 01:50:28 -0300

    Add MainApp widget file with basic theme setup

---
+++ lib/components/main_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/pages/intro_page/intro_page.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Grocery App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          textTheme: GoogleFonts.notoSansTextTheme(
+            Theme.of(context).textTheme.apply(),
+          ),
+        ),
+        home: IntroPage(),
+      );
+  }
+}
