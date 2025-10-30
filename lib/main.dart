50688ade4a2b7fcaae29b5bfa6b13358785f52e3
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-10-30 11:37:13 -0300

    Change runApp to use ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/rendering.dart';
-import 'package:grocery_app/components/main_app.dart';
+import 'package:flutter/rendering.dart';
+import 'package:grocery_app/components/my_app.dart';
 import 'package:grocery_app/models/cart_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  //debugPaintSizeEnabled = true;
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+  debugPaintSizeEnabled = true;
+  runApp(ChangeNotifierProvider(
       create: (context) => CartModel(),
-      child: MainApp(),
-    );
-  }
+      child: MyApp(),
+    )
+  );
 }
