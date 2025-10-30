a0a70506835175eeb5de8b34d0bc34bd13dbbccc
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-10-30 11:43:19 -0300

    Add GroceryItem type file

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,16 @@
+import 'dart:core';
+import 'dart:ui';
+
+class GroceryItem {
+  final String name;
+  final String price;
+  final String image;
+  final Color color;
+
+  GroceryItem({
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.color,
+  });
+}
