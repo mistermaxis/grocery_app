babf757eb4e14aa7cb41228124790fb0b24db810
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-10-30 11:44:40 -0300

    Update test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:grocery_app/main.dart';
+import 'package:grocery_app/components/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
