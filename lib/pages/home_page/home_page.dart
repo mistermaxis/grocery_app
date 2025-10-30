a54ceb8367aedcfdea15c130dbe22281a5b8bfa7
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-10-30 11:42:09 -0300

    Move home_page.dart to a different folder

---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/pages/home_page/components/home_divider.dart';
+import 'package:grocery_app/pages/home_page/components/home_header.dart';
+import 'package:grocery_app/pages/home_page/components/home_lead_in.dart';
+import 'package:grocery_app/pages/home_page/components/home_subtitle.dart';
+import 'package:grocery_app/pages/home_page/components/payment_button.dart';
+import 'package:grocery_app/pages/home_page/components/product_list/product_list.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: PaymentButton(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40),
+            HomeLeadIn(),
+            SizedBox(height: 4),
+            HomeHeader(),
+            SizedBox(height: 8),
+            HomeDivider(),
+            HomeSubtitle(),
+            ProductList(),
+          ], // children
+        ),
+      ),
+    );
+  }
+}
